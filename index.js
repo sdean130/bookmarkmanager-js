5c46a9e63bcd55d572f2916e17c73541778c3269
Seth <seth_dean@mymail.eku.edu>
2023-12-12 00:32:39 -0500

    JS to add cards to display bookmarks

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -28,5 +28,35 @@ addForm.addEventListener("submit", event => {
     })
     .then(() => {
         addForm.reset();
+        showCard();
     })
-})
+});
+
+function generateTemplate(response, id) {
+    return `<div class="card">
+                <p class="title">${response.title}</p>
+                <div class="sub-information">
+                    <p>
+                        <span class="category ${response.category}">${response.category[0].toUpperCase()}${response.category.slice(1)}</span>
+                    </p>
+                    <a href="${response.link}" target="_blank"><i class="bi bi-box-arrow-up-right website"></i></a>
+                    <a href="https://www.google.com/search?q=${response.title}" target="_blank"><i class="bi bi-google search"></i></a>
+                    <span><i class="bi bi-trash delete" data-id="${response.id}"></i></span>
+                </div>
+            </div>`;
+}
+
+const cards = document.querySelector(".cards");
+function showCard() {
+    cards.innerHTML = "";
+    getDocs(colRef)
+        .then(data => {
+            data.docs.forEach(document => {
+            cards.innerHTML += generateTemplate(document.data(), document.id);
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        }) 
+}
+
